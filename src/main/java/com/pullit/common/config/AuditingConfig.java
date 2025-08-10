acf8291025af5c42347f0f30d00943690c68bd68
parksorry96 <pjs96525525@gmail.com>
2025-08-11 00:36:58 +0900

    feat: 공용 annotation 생성

--- src/main/java/com/pullit/common/config/AuditingConfig.java
+++ src/main/java/com/pullit/common/config/AuditingConfig.java
@@ -4,30 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 
-import java.util.Optional;
-
+/**
+ * JPA Auditing 설정
+ * BaseEntity의 createdBy, updatedBy 필드를 자동으로 관리
+ */
 @Configuration
 @EnableJpaAuditing
 public class AuditingConfig {
 
     @Bean
-    public AuditorAware<String> auditorProvider(){
+    public AuditorAware<String> auditorProvider() {
         return new AuditorAwareImpl();
     }
-
-    public Optional<String> getCurrentAuditor(){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if(authentication == null || !authentication.isAuthenticated()){
-            return Optional.of("SYSTEM");
-        }
-
-        //TODO : UserDetails에서 추출하여 반환
-
-        return Optional.of(authentication.getName());
-    }
-
 }
