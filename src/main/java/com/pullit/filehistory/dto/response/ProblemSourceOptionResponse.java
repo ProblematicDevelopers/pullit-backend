b069fd0859ae1446e6677c096ad36255976af42d
kkj <48651886+AAgami@users.noreply.github.com>
2025-08-18 00:00:37 +0900

    feat: 문제가공/교과서,CBT유형선택 관련 api, dto 추가

---
+++ src/main/java/com/pullit/filehistory/dto/response/ProblemSourceOptionResponse.java
@@ -0,0 +1,8 @@
+package com.pullit.filehistory.dto.response;
+
+// code: "CBT || TEXTBOOK"
+// name: 표시명
+// description 한줄 설명
+public record ProblemSourceOptionResponse(String code, String name, String description) {
+
+}
