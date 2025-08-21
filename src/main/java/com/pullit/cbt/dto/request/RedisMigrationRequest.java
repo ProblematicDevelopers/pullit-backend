f1519c75a62e861a3df08e48d76a1df44ad35b11
ddanggoos <hanck9595@gmail.com>
2025-08-21 14:15:43 +0900

    fIx: cbt 시간 저장 설정

--- src/main/java/com/pullit/cbt/dto/request/RedisMigrationRequest.java
+++ src/main/java/com/pullit/cbt/dto/request/RedisMigrationRequest.java
@@ -12,6 +12,6 @@ public class RedisMigrationRequest {
     private Map<String, Integer> questionTimes; // 문제별 소요 시간
     private Map<String, Integer> questionVisits; // 문제별 방문 횟수
     private Map<String, String> questionAnswers; // 문제별 답변
-    private Integer totalTime; // 총 시험 시간
+    private Integer remainingTime; // 남은 시험 시간
     private String status; // 시험 상태
 }
