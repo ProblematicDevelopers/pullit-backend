b9b9afeac9e61f0255499ae803ccdde2701176d5
parksorry96 <pjs96525525@gmail.com>
2025-08-25 16:46:07 +0900

    feat: school

--- src/main/java/com/pullit/classes/controller/SchoolController.java
+++ src/main/java/com/pullit/classes/controller/SchoolController.java
@@ -1,4 +1,20 @@
 package com.pullit.classes.controller;
 
+import com.pullit.exam.service.ExamSearchService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/schools")
+@Slf4j
+@Tag(name = "School", description = "학교 관련 API")
+@RequiredArgsConstructor
 public class SchoolController {
+
+    private final ExamSearchService examSearchService;
+
+
 }
