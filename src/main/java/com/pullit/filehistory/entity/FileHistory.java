181903d2ea4e1cc182fe8f7f1cd26c6dbe789e27
kkj <48651886+AAgami@users.noreply.github.com>
2025-08-27 09:42:01 +0900

    feat: filehistory 칼럼 수정

--- src/main/java/com/pullit/filehistory/entity/FileHistory.java
+++ src/main/java/com/pullit/filehistory/entity/FileHistory.java
@@ -1,12 +1,13 @@
 package com.pullit.filehistory.entity;
 
+import com.pullit.common.file.entity.FileMetadata;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.sql.Timestamp;
 
 @Entity
-@Table(name="file_history")
+@Table(name = "file_history")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -18,18 +19,29 @@ public class FileHistory {
     @Column(name = "file_history_id", nullable = false)
     private Long id;
 
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id", nullable = false)
+    private FileMetadata fileMetadataId;
+
     @Column(name = "subject_id", nullable = false)
     private Long subjectId;
 
     @Column(name = "file_history_name")
     private String fileHistoryName;
 
-    @Column(name = "origin_pdf_url")
-    private String originPdfUrl;
+    @Column(name = "img_count")
+    private Long imgCount;
+
+    @Column(name = "img_order")
+    private String imgOrder;
 
     @Column(name = "created_by")
     private String createdBy;
 
     @Column(name = "created_date")
     private Timestamp createdDate;
+
+    @Column(name = "updated_date")
+    private Timestamp updatedDate;
+
 }
