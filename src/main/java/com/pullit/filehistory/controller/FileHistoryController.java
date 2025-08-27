55f5357ba45e1f0ce22493ff65caaa049e4e3ac0
kkj <48651886+AAgami@users.noreply.github.com>
2025-08-27 14:23:31 +0900

    comment: 로직변경으로 전부 주석처리(임시)

--- src/main/java/com/pullit/filehistory/controller/FileHistoryController.java
+++ src/main/java/com/pullit/filehistory/controller/FileHistoryController.java
@@ -1,22 +1,11 @@
 package com.pullit.filehistory.controller;
 
-import com.pullit.common.constants.ServiceConstants;
-import com.pullit.common.dto.response.ApiResponse;
-import com.pullit.exam.entity.Exam;
-import com.pullit.filehistory.dto.response.ProblemSourceOptionResponse;
 import com.pullit.filehistory.service.FileHistoryService;
-import com.pullit.item.dto.response.SubjectResponse;
-import com.pullit.item.service.SubjectService;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -25,20 +14,20 @@ import java.util.List;
 @Tag(name = "File History", description = "문제 가공 파일 관련 API")
 public class FileHistoryController {
     private final FileHistoryService fileHistoryService;
-    private final SubjectService subjectService;
 
-    //TODO: 교과서 선택시 교과서 리스트 반환
-    @GetMapping("/textbook")
-    @Operation(summary = "교과서 선택", description = "교과서 리스트를 출력합니다")
-    public ResponseEntity<ApiResponse<List<SubjectResponse>>> findAllTextbook() {
-        List<SubjectResponse> subjects = subjectService.findAllSubjectsOnly();
-        return ResponseEntity.ok(ApiResponse.success(subjects));
-    }
 
-    //TODO: pdf 업로드시 file history 저장
+
 
 }
-
+//    //TODO: 교과서 선택시 교과서 리스트 반환
+//    @GetMapping("/textbook")
+//    @Operation(summary = "교과서 선택", description = "교과서 리스트를 출력합니다")
+//    public ResponseEntity<ApiResponse<List<SubjectResponse>>> findAllTextbook() {
+//        List<SubjectResponse> subjects = subjectService.findAllSubjectsOnly();
+//        return ResponseEntity.ok(ApiResponse.success(subjects));
+//    }
+//
+//    //TODO: pdf 업로드시 file history 저장
 // 참고: 교과서 선택으로 바로 빠지기때문에 해당 함수는 주석 처리만 해둠
 //    @GetMapping("/source")
 //    @Operation(summary = "교과서 OR CBT 모드 선택", description = "문제 가공 모드 선택 페이지. 교과서 혹은 CBT 둘중 하나를 선택합니다. ")
