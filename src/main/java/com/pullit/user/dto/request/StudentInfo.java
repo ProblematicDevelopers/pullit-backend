98637b1852c43b8658c4319b2250644a70db8007
leenanhui <nanhui0622@gmail.com>
2025-09-02 11:31:19 +0900

    nani

--- src/main/java/com/pullit/user/dto/request/StudentInfo.java
+++ src/main/java/com/pullit/user/dto/request/StudentInfo.java
@@ -8,4 +8,6 @@ public class StudentInfo {
     private Long classGroupId;
     private Long studentNo;
     private StringCodeNamePair grade;
+    private Long schoolId;  // 학교 ID 추가
+    private String schoolName;  // 학교명 추가
 }
